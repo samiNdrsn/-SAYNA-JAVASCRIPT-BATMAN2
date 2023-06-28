14175bc705a006aa86d404935c993306515a7a2f
samiNdrsn <129782460+samiNdrsn@users.noreply.github.com>
2023-06-28 21:58:55 +0300

    Ajout

---
+++ game.js
@@ -0,0 +1,5 @@
+const bouton = document.getElementById('bouton');
+const cadre = document.getElementById('cadre');
+bouton.addEventListener=('click',fonction(){
+    cadre.style.display='block';
+});
